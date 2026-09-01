73b24d758f95a6329712bb081090885106dc0cfd
anth-id <anthon02@hotmail.com>
2026-09-01 20:28:02 +0200

    Added a calc function and use calculation functions inside it

--- calculator.py
+++ calculator.py
@@ -10,31 +10,33 @@ def mult(x,y):
 def div(x, y):
     return x/y
 
+def calc():
+    while True:
+        print("Select operator: 1.Add, 2.Subtract, 3.Multi, 4.Division \n If you want to stop the program: Press X")
+        choice = input("Enter 1, 2, 3 or 4: ")
+        if choice == "x":
+            break
+        #break choice on x
 
-while True:
-    print("Select operator: 1.Add, 2.Subtract, 3.Multi, 4.Division \n If you want to stop the program: Press X")
-    choice = input("Enter 1, 2, 3 or 4: ")
-    if choice == "x":
-        break
-    #break choice on x
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+        #try while loop so the program doesnt close when finshed. 
+        #Update: when only "While True" it ran infinite. Need ot move input into the while loop.
 
-    #try while loop so the program doesnt close when finshed. 
-    #Update: when only "While True" it ran infinite. Need ot move input into the while loop.
+        if choice == "1":
+            print(add(num1, num2))
+        elif choice == "2":
+            print(sub(num1, num2))
+        elif choice == "3":
+            print(mult(num1, num2))
+        elif choice == "4":
+            print(div(num1,num2))
 
-    if choice == "1":
-        print(num1+num2)
-    elif choice == "2":
-        print(num1-num2)
-    elif choice == "3":
-        print(num1*num2)
-    elif choice == "4":
-        print(num1/num2)
+        else:
+            print(f"{choice} is not a choice")
 
-    else:
-        print(f"{choice} is not a choice")
-
+        print("--------------------")
+calc()
     
 
